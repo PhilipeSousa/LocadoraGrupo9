bf8a9868b6b54421d3643e2675b6bee5f67d0daa
Philipe de Sousa <78869177+PhilipeSousa@users.noreply.github.com>
2022-03-24 12:54:47 -0300

    Update Locatario

--- Trabalho/src/gerenciaLocatarios/Locatario.java
+++ Trabalho/src/gerenciaLocatarios/Locatario.java
@@ -1,49 +1,145 @@
-package gerenciaLocatarios;
+package GerenciadorLocatario;
+
+import javax.swing.JOptionPane;
 
 public class Locatario {
-
-     String logradouro;
-     int numero;
-     String complemento;
-     String bairro;
-     String cidade;
-     String cep;
-
-     Endereco endereco = new Endereco(logradouro, numero, complemento, bairro, cidade, cep);
-
-     String celular;
-     String email;
-
-    public Locatario(String celular, String email, String logradouro, int numero, String complemento, String bairro,
-            String cidade, String cep) {
-        this.celular = celular;
-        this.email = email;
-
-        this.logradouro = endereco.getLogradouro();
-        this.logradouro = logradouro;
-
-        this.numero = endereco.getNumero();
-        this.numero = numero;
-
-        this.complemento = endereco.getComplemento();
-        this.complemento = complemento;
-
-        this.bairro = endereco.getBairro();
-        this.bairro = bairro;
-
-        this.cidade = endereco.getCidade();
-        this.cidade = cidade;
-
-        this.cep = endereco.getCep();
-        this.cep = cep;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
+	String email;
+	String telefone;
+	Endereco endereco;
+	static Locatario[] loc = new Locatario[0];
+	
+	public String getEmail() {
+		return email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	private static void cadastrarLocatario(Locatario locat) {
+		/*Locatario[] temp = new Locatario[loc.length + 1];
+		for (int i = 0; i < loc.length; i++) {
+			temp[i] = loc[i];
+		}
+		temp[loc.length] = locat;
+		loc = temp;
+		JOptionPane.showMessageDialog(null, "Locat�rio cadastrado com sucesso!");*/
+	}
+	
+	public static void cadastrarPessoaFisica() {
+		/*String nome = JOptionPane.showInputDialog("Nome: ");
+		String estadoCivil = JOptionPane.showInputDialog("Estado Civil: ");
+		String cpf = JOptionPane.showInputDialog("CPF: ");
+		String email = JOptionPane.showInputDialog("E-mail: ");
+		String telefone = JOptionPane.showInputDialog("Telefone: ");
+		String cidade1 = JOptionPane.showInputDialog("Cidade: "); 
+		String estado1 = JOptionPane.showInputDialog("Estado: ");
+		String rua1 = JOptionPane.showInputDialog("Rua: ");
+		String numero1 = JOptionPane.showInputDialog("N�mero: ");
+		String complemento1 = JOptionPane.showInputDialog("Complemento: ");
+		String bairro1 = JOptionPane.showInputDialog("Bairro: ");
+		String cep1 = JOptionPane.showInputDialog("CEP: ");
+		PessoaFisica pfs = new PessoaFisica(nome, estadoCivil, cpf, email, telefone);
+		pfs.setEndereco(new Endereco(cidade1, estado1, rua1, numero1, complemento1, bairro1, cep1));
+		cadastrarLocatario(pfs);*/
+	}
+	
+	public static void cadastrarPessoaJuridica() {
+		/*String cnpj = JOptionPane.showInputDialog("CNPJ: ");
+		String nomeSocial = JOptionPane.showInputDialog("Nome social: ");
+		String email = JOptionPane.showInputDialog("E-mail: ");
+		String telefone = JOptionPane.showInputDialog("Telefone: ");
+		PessoaJuridica pjs = new PessoaJuridica(cnpj, nomeSocial, email, telefone);	
+		cadastrarLocatario(pjs);*/
+	}
+	
+	public static void imprimirLocatario() {
+		/*for (int x = 0; x < loc.length; x++) {
+			if (loc[x] instanceof PessoaFisica) {
+				PessoaFisica temp = (PessoaFisica) loc[x];
+				String resposta = temp.getNome() + "\n" +
+						temp.estadoCivil + "\n" +
+						temp.cpf + "\n" +
+						loc[x].getEmail() + "\n" +
+						loc[x].getTelefone() + "\n" +
+						loc[x].endereco.getCidade() + "\n" +
+						loc[x].endereco.getEstado() + "\n" +
+						loc[x].endereco.getRua() + "\n" +
+						loc[x].endereco.getNumero() + "\n" +
+						loc[x].endereco.getComplemento() + "\n" +
+						loc[x].endereco.getBairro() + "\n" +
+						loc[x].endereco.getCep();
+				JOptionPane.showMessageDialog(null, resposta);
+			}
+		}*/
+	}
+	
+	public static Locatario buscarLocatario() {	
+		/*JOptionPane.showMessageDialog(null, "Realizar busca de locat�rio ");
+		String nomeCpf = JOptionPane.showInputDialog("Digite um nome ou CPF: ");
+		
+		for (int x = 0; x < loc.length; x++) {
+			if (loc[x] instanceof PessoaFisica) {
+				PessoaFisica temp = (PessoaFisica) loc[x];
+				String nomeLocatario = temp.getNome();
+				String cpfLocatario = temp.getCpf();
+				if (cpfLocatario.equals(nomeCpf)| nomeLocatario.equals(nomeCpf)) {
+					return loc[x];
+				} 
+			}
+		}
+		return null;*/
+	}
+	
+	public static void alterarLocatario() {
+		// Implementar o m�todo alterar locat�rio
+	}
+	
+	
+	public static void pesquisarLocatario() {
+		/*String nomeCpf = JOptionPane.showInputDialog("Digite um nome ou CPF: ");
+		boolean indicador = false;
+		for (int x = 0; x < loc.length; x++) {
+			if (loc[x] instanceof PessoaFisica) {
+				PessoaFisica temp = (PessoaFisica) loc[x];
+				String nomeLocatario = temp.getNome();
+				String cpfLocatario = temp.getCpf();
+				if (cpfLocatario.equals(nomeCpf)| nomeLocatario.equals(nomeCpf)) {
+					JOptionPane.showMessageDialog(null, "Est� na lista!");
+					indicador = true;
+				} 
+			}
+		}
+		if (indicador == false) {
+			JOptionPane.showMessageDialog(null, "N�o est� na lista!");
+		}*/
+	}
+	
+	public static void removerLocatario() {
+		/*Locatario elementoASerRemovido = buscarLocatario();
+		Locatario[] newLoc = null;
+		if (elementoASerRemovido instanceof PessoaFisica) {
+			for (int i = 0; i < loc.length-1; i++) {
+				if (loc[i] == elementoASerRemovido) {
+					newLoc = new Locatario[loc.length - 1];
+					for (int index = 0; index < i; index++) {
+						newLoc[index] = loc[index];
+					}
+					for (int j = i; j < loc.length - 1; j++) {
+						newLoc[j] = loc[j+1];
+					}
+					loc = newLoc;
+					break;
+				}
+			}
+			JOptionPane.showMessageDialog(null, "Locat�rio removido!");
+		} else if (elementoASerRemovido == null) {
+			JOptionPane.showMessageDialog(null, "Locatario n�o encontrado!");
+		}
+	}*/
 }
