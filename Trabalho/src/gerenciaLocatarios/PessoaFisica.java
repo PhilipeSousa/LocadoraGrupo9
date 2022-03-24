fd8c68d69fc7c21b1618f9c71eb794e9c0822e1b
Philipe de Sousa <78869177+PhilipeSousa@users.noreply.github.com>
2022-03-24 13:01:05 -0300

    Update PF

--- Trabalho/src/gerenciaLocatarios/PessoaFisica.java
+++ Trabalho/src/gerenciaLocatarios/PessoaFisica.java
@@ -1,29 +1,34 @@
-package gerenciaLocatarios;
+package GerenciadorLocatario;
 
 public class PessoaFisica extends Locatario {
-     String nomeCompleto;
-     String cpf;
-     String estadoCivil;
-
-    public PessoaFisica(String celular, String email, String logradouro, int numero, String complemento,
-            String bairro, String cidade, String cep, String nomeCompleto, String cpf, String estadoCivil) {
-
-        super(celular, email, logradouro, numero, complemento, bairro, cidade, cep);
-        this.nomeCompleto = nomeCompleto;
-        this.cpf = cpf;
-        this.estadoCivil = estadoCivil;
-    }
-
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getEstadoCivil() {
-        return estadoCivil;
-    }
-
+	String nome;
+	String estadoCivil;
+	String cpf;
+	
+	public PessoaFisica(String nome2, String estadoCivil2, String cpf2, String email, String telefone) {
+		this.nome = nome2;
+		this.estadoCivil = estadoCivil2;
+		this.cpf = cpf2;
+		this.email = email;
+		this.telefone = telefone;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
 }
